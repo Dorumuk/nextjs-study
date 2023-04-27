7de7361ad650a2ab1883defeee827e7d52dc7faf
Dorumuk <haulinef@gmail.com>
2023-04-27 13:29:36 +0900

    Learn SEO in Nextjs v13

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -12,6 +12,12 @@ export default function PantsPage({ params }: Props) {
   return <h1>{params.slug} 제품 설명 페이지</h1>;
 }
 
+export function generateMetadata({ params }: Props) {
+  return {
+    title: `제품의 이름: ${params.slug}`
+  }
+}
+
 // 유용함
 export function generateStaticParams() {
   const products = ['pants', 'skirt'];
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,16 @@
 import Link from "next/link";
 
+const products = ['shirt', 'pants', 'skirt', 'shoes'];
+
 export default function ProductsPage() {
   return (
     <>
       <h1>제품 소개 페이지!</h1>
-      <section>
-        <ul>
-          <li><Link href='/products/shirt'>shirt</Link></li>
-          <li><Link href='/products/pants'>pants</Link></li>
-          <li><Link href='/products/skirt'>skirt</Link></li>
-          <li><Link href='/products/shoes'>shoes</Link></li>
-        </ul>
-      </section>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}><Link href={`/products/${product}`}>{product}</Link></li>
+        ))}
+      </ul>
     </>
   );
 }
